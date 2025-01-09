66bea86e1c5007cd8dc9a46044672338d3fa93e6
hoken92 <hkenneth92@gmail.com>
2025-01-09 10:00:57 -0800

    refactored create profile

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,22 +1,29 @@
 import { Button } from "@/components/ui/button";
+import FormContainer from "@/components/form/FormContainer";
+import FormInput from "@/components/form/FormInput";
+import SubmitButton from "@/components/form/Buttons";
 
-const createProfileAction = async (formData: FormData) => {
+const createProfileAction = async (prevState: any, formData: FormData) => {
   // form action that will send us the firstName value
   "use server";
   const firstName = formData.get("firstName") as string;
   console.log(firstName);
+  return { message: "Profile Created." };
 };
 
 function CreateProfile() {
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
-      <div className="border p-8 rounded-md max-w-lg">
-        <form action={createProfileAction}>
-          <Button type="submit" size="lg">
-            Create Profile
-          </Button>
-        </form>
+      <div className="border p-8 rounded-md">
+        <FormContainer action={createProfileAction}>
+          <div className="grid grid-cols-2 gap-4 mt-4">
+            <FormInput type="text" name="firstName" label="First Name" />
+            <FormInput type="text" name="lastName" label="Last Name" />
+            <FormInput type="text" name="username" label="Username" />
+          </div>
+          <SubmitButton text="Create Profile" className="mt-8" />
+        </FormContainer>
       </div>
     </section>
   );
